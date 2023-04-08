bd9c8aefad75c16d20ba56bedc64838c55884229
den669 <71308629+den669@users.noreply.github.com>
2023-04-08 23:33:31 +0300

    Add files via upload

---
+++ laba7/ClassLibrary1/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace AntSimulation
+{
+    public class Ant
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Ant(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Move()
+        {
+            
+            Console.WriteLine($"Мурашка рухається на координатах ({X}, {Y})");
+        }
+    }
+
+    public class AntColony
+    {
+        private Ant[] ants;
+        private Random random;
+
+        public AntColony(int size)
+        {
+            ants = new Ant[size];
+            random = new Random();
+            InitializeAnts();
+        }
+
+        private void InitializeAnts()
+        {
+            
+           
+            for (int i = 0; i < ants.Length; i++)
+            {
+                int x = random.Next(0, 100); 
+                int y = random.Next(0, 100); 
+                ants[i] = new Ant(x, y);
+            }
+        }
+
+        public void Simulate()
+        {
+            
+            Console.WriteLine("Імітація руху мурашок в колонії");
+            foreach (var ant in ants)
+            {
+                ant.Move();
+            }
+        }
+    }
+}
---
+++ laba7/laba7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AntSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            AntColony colony = new AntColony(10); 
+
+            colony.Simulate();
+
+            Console.WriteLine("Програма завершила роботу");
+        }
+    }
+}
